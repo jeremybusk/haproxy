d58c160c0fef63ce167fc40eb00d119d232dcf91
Jeremy Busk <jeremybusk@gmail.com>
2020-02-06 16:53:31 -0700

    Create prep.sh

---
+++ prep.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+# copy haproxy-simple.cfg and gen cert for it
+
+dnf install haproxy
+cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.o
+cp haproxy-simple.cfg /etc/haproxy/haproxy.cfg
+mkdir /etc/haproxy/pki
+cd /etc/haproxy/pki
+openssl req -subj '/CN=example/O=Utah/C=US' -new -newkey rsa:2048 -sha256 -days 3650 -nodes -x509 -keyout server.key -out server.crt
+cat server.crt server.key > /etc/haproxy/pki/server.pem
+haproxy -c -V -f /etc/haproxy/haproxy.cfg
+systemctl start haproxy
+systemctl enable haproxy
