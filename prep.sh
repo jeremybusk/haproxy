deb01be6b26efb8a1800038251a175fa7ece26e7
Jeremy Busk <jeremybusk@gmail.com>
2020-02-06 20:29:20 -0700

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -13,3 +13,4 @@ haproxy -c -V -f /etc/haproxy/haproxy.cfg
 systemctl start haproxy
 systemctl enable haproxy
 curl -k https://www.example.com:10443 --resolve www.example.com:10443:127.0.0.1  # 127.0.0.1 is your hap vserver ip
+curl -k http://www.example.com:10080 --resolve www.example.com:10080:127.0.0.1
