384f1f87d39b9734651d90c368f5752591f2c668
Jeremy Busk <jeremybusk@gmail.com>
2020-03-02 01:58:14 +0000

    edit notify script

--- osfiles/usr/local/bin/keepalived_notify.sh
+++ osfiles/usr/local/bin/keepalived_notify.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env /bash
-echo "Hello"
+
+if [ "$3" = "MASTER" ]; then
+  echo "Master" >> /tmp/haproxy.log
+  # touch /var/run/keepalived/$2
+else
+  echo "Not Master" >> /tmp/haproxy.log
+  # rm /var/run/keepalived/$2
+fi
