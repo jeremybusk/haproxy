0a30e27c3b6e44e5c90b7440c40846a1271c94d5
Jeremy Busk <jeremybusk@gmail.com>
2020-03-21 02:15:20 +0000

    update install for ssh

--- install.sh
+++ install.sh
@@ -1,39 +1,64 @@
 #!/usr/bin/env bash
 # set -exo pipefail
 set -x
-CNAME1="ubuntu"
-IMAGE1="ubuntu:bionic"
-CNAME2="centos"
-IMAGE2="images:centos/8"
+hap1="hap1"
+hap2="hap2"
+image="ubuntu:bionic"
+ssh_pass="rootit"
+# uuid=$(uuidgen)
+hap1=$(uuidgen)
+hap2=$(uuidgen)
+nginx=$(uuidgen)
+# CNAME2="centos"
+# image="images:centos/8"
+current_user=$(whoami)
 
-echo "Hello there"
-# snap install lxd --edge
-
-# if [ ! -z ${LINT} ]; then echo "Hello, world!"; fi
-
-echo "start"
 sudo apt remove -y --purge lxd lxd-client;
-sudo snap install lxd --stable;
-sudo lxd waitready;
-sudo lxd init --auto;
-echo "finish"
+sudo snap install lxd --stable  # --edge
+sudo lxd waitready
+sudo lxd init --auto
+
+# sudo usermod --append --groups lxd "${current_user}"
 # sudo chmod 666 /var/snap/lxd/common/lxd/unix.socket;  # avoids need for sudo on read
-sudo lxc list
 
-sudo lxc launch ${IMAGE1} ${CNAME1};
-sudo lxc exec ${CNAME1} -- sh -c "lsb_release -a || cat /etc/redhat-release";
-sudo lxc launch ${IMAGE2} ${CNAME2};
-sudo lxc exec ${CNAME2} -- sh -c "lsb_release -a || cat /etc/redhat-release";
-sudo lxc exec ${CNAME2} -- sh -c "ping -c 4 ubuntu.lxd";
-sudo lxc exec ${CNAME2} -- sh -c "ping -c 4 ubuntu";
-sudo lxc exec ${CNAME2} -- sh -c "ping -c 4 ${CNAME1}";
-echo "sleep 10"
+
+# sudo lxc launch ${IMAGE1} ${CNAME1};
+# sudo lxc exec ${CNAME1} -- sh -c "lsb_release -a || cat /etc/redhat-release";
+# sudo lxc launch ${IMAGE2} ${CNAME2};
+# sudo lxc exec ${CNAME2} -- sh -c "lsb_release -a || cat /etc/redhat-release";
+# sudo lxc exec ${CNAME2} -- sh -c "ping -c 4 ubuntu.lxd";
+# sudo lxc exec ${CNAME2} -- sh -c "ping -c 4 ubuntu";
+# sudo lxc exec ${CNAME2} -- sh -c "ping -c 4 ${CNAME1}";
+# echo "sleep 10"
+# sleep 10
+# sudo lxc exec ${CNAME2} -- sh -c "ping -c 4 ubuntu.lxd";
+# sudo lxc exec ${CNAME2} -- sh -c "ping -c 4 ubuntu";
+# sudo lxc exec ${CNAME2} -- sh -c "ping -c 4 ${CNAME1}";
+# # snap install lxd --stable
+# sudo lxc list
+# lxc list
+
+
+containers[0]=hap1
+containers[1]=hap2
+containers[2]=nginx
+for ${c} in "${containers[@]}"; do
+    sudo lxc launch ${IMAGE1} ${CNAME1};
+done
+
 sleep 10
-sudo lxc exec ${CNAME2} -- sh -c "ping -c 4 ubuntu.lxd";
-sudo lxc exec ${CNAME2} -- sh -c "ping -c 4 ubuntu";
-sudo lxc exec ${CNAME2} -- sh -c "ping -c 4 ${CNAME1}";
-# snap install lxd --stable
-sudo lxc list
-lxc list
 
-whoami
+for ${c} in "${containers[@]}"; do
+    sudo lxc exec ${CNAME1} -- sh -c "lsb_release -a || cat /etc/redhat-release";
+    sudo lxc exec ${c} sed -i 's/^PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+    sudo lxc exec ${c} systemctl restart ssh 
+    sudo lxc exec ${c} ssh-keygen -f ~/.ssh/id_app -N ''
+    sudo lxc exec ${c} ssh-copy-id -i ~/.ssh/id_app user@host
+    for ${ssh_host} in "${containers[@]}"; do
+	if [ "${ssh_host}" != "${c}" ]; then
+	    sudo lxc exec ${c} ping -c 2 ${ssh_host}
+    	    sudo lxc exec ${c} sshpass -p ${ssh_pass} ssh-copy-id -i ~/.ssh/id_ed25519 root@${ssh_host}
+            sudo lxc exec ssh root@${ssh_host} hostname
+        fi
+    done
+done
