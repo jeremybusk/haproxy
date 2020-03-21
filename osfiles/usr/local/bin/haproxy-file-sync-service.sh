556b6daa840e9f359654c5e9b043404db402ef02
Jeremy Busk <jeremybusk@gmail.com>
2020-03-21 20:11:01 +0000

    fix hap and ka configs

--- osfiles/usr/local/bin/haproxy-file-sync-service.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-while [ true ]; do
- sleep 15  # sync every 15 seconds
- # do what you need to here
-echo "running sync"
-./osync.sh --initiator=/jtmp --target=ssh://root@hap2c1:22//jtmp
-done
